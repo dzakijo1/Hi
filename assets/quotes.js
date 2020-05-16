747d7281318352ac17c99335121ab8776a63fa40
dzakijo1 <47619660+dzakijo1@users.noreply.github.com>
2020-05-17 03:36:48 +0700

    Add files via upload

---
+++ assets/quotes.js
@@ -0,0 +1,10 @@
+// prettier-ignore
+export default [
+    { author: "Dzaki_Jo", quotes: "Whatever happens, keep breathing" },
+    { author: "Dzaki_Jo", quotes: "Nurlela Alfida Maghfiroh:*" },
+    { author: "Dzaki_Jo", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "Dzaki_Jo", quotes: "I love you." },
+    { author: "Dzaki_Jo", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "Dzaki_Jo", quotes: "I love you." },
+    { author: "Dzaki_Jo", quotes: "I'm sorry for loving you." },
+];
---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["Elakuu", "Babe", "Sayang", "Ma Luv"],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Good Day",
+    morning: "Good Morning",
+    night: "Good Night"
+  }
+};
